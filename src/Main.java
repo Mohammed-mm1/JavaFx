239b1f686397829c7989e7fb7ae22b6e6a90265e
Mohammed-mm1 <mohammed707ksa1@gmail.com>
2025-08-12 22:15:00 +0300

    empty page

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.show();
-
     }
-
     public static void main(String[] args) {
         launch(args);
     }
